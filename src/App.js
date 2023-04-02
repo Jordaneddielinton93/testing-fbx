df2d518aefe7d0dfa5e50b280a5f1ec0c507b6da
Jordan eddie linton <jordaneddielinton93@hotmail.co.uk>
2023-04-02 17:31:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls, useFBX } from "@react-three/drei";
+import { Suspense } from "react";
+
+const Scene = () => {
+  const fbx = useFBX("./Walking.fbx");
+
+  return <primitive object={fbx} scale={1} />;
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Scene />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
